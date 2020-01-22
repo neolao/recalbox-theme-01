b8998d8180d36627ca8625b88d653422d84f41d0
neolao <contact@neolao.com>
2020-01-22 11:58:17 +0000

    up

--- commands/updateGamelists.js
+++ commands/updateGamelists.js
@@ -1,3 +1,44 @@
+const { readdir } = require("fs").promises;
+const { extname, basename } = require("path");
+const loadGamelist = require("../utils/loadGamelist");
+const saveGamelist = require("../utils/saveGamelist");
+
+function getGameImagePath(game) {
+  const extension = extname(game.path);
+  const filename = basename(game.path, extension);
+  return `./theme-neolao-01/${filename}.png`;
+}
+
+async function updateSystemGamelist(directoryPath) {
+  process.stdout.write(`System ${directoryPath}:\n`);
+  const gamelistPath = `${directoryPath}/gamelist.xml`;
+
+  let games;
+  try {
+    games = await loadGamelist(gamelistPath);
+  } catch (error) {
+    process.stdout.write("No gamelist.xml\n\n");
+    return;
+  }
+
+  for (const game of games) {
+    game.image = getGameImagePath(game);
+  }
+
+  try {
+    await saveGamelist(games, gamelistPath);
+  } catch (error) {
+    process.stdout.write(`Unable to save ${gamelistPath}\n`);
+  }
+
+  process.stdout.write("\n");
+}
+
 module.exports = async function updateGamelists() {
-  // update
+  const systemsDirectoryPath = "/recalbox/share/roms";
+  const systemDirectoryNames = await readdir(systemsDirectoryPath);
+  for await (const systemDirectoryName of systemDirectoryNames) {
+    const systemDirectoryPath = `${systemsDirectoryPath}/${systemDirectoryName}`;
+    await updateSystemGamelist(systemDirectoryPath);
+  }
 };
--- utils/loadGamelist.js
+++ utils/loadGamelist.js
@@ -2,6 +2,35 @@ const { readFile } = require("fs").promises;
 const xml2js = require("xml2js");
 const getAbsolutePath = require("./getAbsolutePath");
 
+function normalizeGame(game) {
+  const normalized = {};
+
+  for (const key in game) {
+    if (Array.isArray(game[key]) && game[key].length === 0) {
+      normalized[key] = undefined;
+    } else if (Array.isArray(game[key]) && game[key].length === 1) {
+      const [value] = game[key];
+      normalized[key] = value;
+    } else {
+      normalized[key] = game[key];
+    }
+  }
+
+  return normalized;
+}
+
+function sortByName(game1, game2) {
+  if (game1.name < game2.name) {
+    return -1;
+  }
+
+  if (game1.name > game2.name) {
+    return 1;
+  }
+
+  return 0;
+}
+
 module.exports = async function loadGamelist(filePath) {
   const absoluteFilePath = getAbsolutePath(filePath, process.cwd());
   const parser = new xml2js.Parser();
@@ -9,20 +38,5 @@ module.exports = async function loadGamelist(filePath) {
   const data = await parser.parseStringPromise(xml);
   const games = data.gameList.game;
 
-  return games.map(game => {
-    const normalized = {};
-
-    for (const key in game) {
-      if (Array.isArray(game[key]) && game[key].length === 0) {
-        normalized[key] = undefined;
-      } else if (Array.isArray(game[key]) && game[key].length === 1) {
-        const [value] = game[key];
-        normalized[key] = value;
-      } else {
-        normalized[key] = game[key];
-      }
-    }
-
-    return normalized;
-  });
+  return games.map(normalizeGame).sort(sortByName);
 };
